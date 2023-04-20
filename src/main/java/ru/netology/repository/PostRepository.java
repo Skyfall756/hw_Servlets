cae4ffc5419bf4d163540b7e9f578177c899ccf4
Skyfall756 <110997330+Skyfall756@users.noreply.github.com>
2023-04-20 23:27:50 +0300

    init

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,44 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+// Stub
+public class PostRepository {
+
+    private AtomicLong counterID;
+    private ConcurrentHashMap<Long, Post> posts;
+
+    public PostRepository() {
+        this.posts = new ConcurrentHashMap<>();
+        this.counterID = new AtomicLong(0);
+    }
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.getOrDefault(id, null));
+    }
+
+    public Post save(Post post) {
+        if (posts.containsKey(post.getId())) {
+            posts.replace(post.getId(), post);
+        } else {
+            post.setId(counterID.getAndIncrement());
+            posts.put(post.getId(), post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
+}
